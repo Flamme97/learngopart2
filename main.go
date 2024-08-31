80422062fe1211027cd06679fbc8a14dd19c4a43
obl <obl@danalock.com>
2024-08-31 17:23:34 +0200

    can make post request to create user and adjust stuff.

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/flamme97/rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 
 func main(){
@@ -19,6 +27,27 @@ func main(){
 	portString := os.Getenv("PORT")
 	if portString == ""{
 		log.Fatal("PORT is not found in the environment")
+	}
+
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == ""{
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err!= nil{
+		log.Fatal("can't connect to database")
+	}
+
+	queries := database.New(conn)
+	if queries == nil {
+		log.Fatal("can't create connection to db connection ", err)
+	}
+
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
 	}
 
 	router := chi.NewRouter()
@@ -36,6 +65,7 @@ func main(){
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users",apiCfg.handlerCreateUser)
 
 
 	router.Mount("/v1",v1Router)
@@ -47,7 +77,7 @@ func main(){
 
 	log.Printf("Server starting on port %v", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
