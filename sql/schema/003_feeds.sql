39ea77c354268d368422e5e0be75a29b8e1eca31
obl <obl@danalock.com>
2024-09-01 09:39:47 +0200

    added feeds

--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -10,3 +10,17 @@ CREATE TABLE feeds (
 
 -- +goose Down
 DROP TABLE feeds;
+
+-- name: GetNextFeedToFetch :one
+
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds 
+SET last_fetched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
+
